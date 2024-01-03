f269f26b7ded5da1e510ca13b6be321d59bf1c84
Olimpia Bobu <olimpiamiruna@gmail.com>
2024-01-03 18:31:58 +0200

    tastatura minunata, trb sa o modific

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,51 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Main extends JFrame {
+
+public class Main extends JFrame  {
 
 
     public static void main(String[] args) {
+        JLabel label = new JLabel();
+        label.setText("T a s t a t u r a");
+        label.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+        label.setHorizontalAlignment(0);
+        label.setVerticalAlignment(0);
 
-        JLabel label  = new JLabel();
-        label.setText("Hi");
 
         JPanel tastatura = new JPanel();
-        tastatura.setBounds(0,0,500,500);
+        tastatura.setBounds(0, 0, 500, 500);
+        tastatura.setLayout(new GridLayout(4, 3, 3, 3));
         Color champagneColor = new Color(247, 231, 206);
         tastatura.setBackground(champagneColor);
 
+        for (int i = 9; i >= 0; i--) {
+            JButton button = new JButton(Integer.toString(i));
+            button.setFocusable(false);
+            int finalI = i;
+            button.addActionListener(e -> System.out.printf("A fost apasata tasta %d\n", finalI));
+            tastatura.add(button);
+        }
+
+        JButton Enterbutton = new JButton();
+        Enterbutton.setText("ENTER");
+        Enterbutton.setFocusable(false);
+        Enterbutton.addActionListener(e -> System.out.println("A fost apasata tasta  enter"));
+        tastatura.add(Enterbutton);
+
+
         JPanel display = new JPanel();
-        display.setBounds(500,0,500,500);
+        display.setBounds(500, 0, 500, 500);
         Color camel = new Color(193, 154, 107);
         display.setBackground(camel);
 
         JPanel cvbutoane = new JPanel();
-        cvbutoane.setBounds(0,500,500,500);
-        Color cf = new Color(166,123,91);
+        cvbutoane.setBounds(0, 500, 500, 500);
+        Color cf = new Color(166, 123, 91);
         cvbutoane.setBackground(cf);
 
         JPanel processor = new JPanel();
-        processor.setBounds(500,0,500,500);
+        processor.setBounds(500, 0, 500, 500);
         Color almond = new Color(230, 221, 216);
         processor.setBackground(almond);
 
@@ -39,6 +59,7 @@ public class Main extends JFrame {
         frame.add(display);
         frame.add(cvbutoane);
         frame.add(processor);
+
     }
 
-}
+    }
